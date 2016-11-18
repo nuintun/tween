78657d90937f506967241104eaef346050d31059
nuintun <nuintun@qq.com>
2016-11-18 11:48:24 +0800

    update files

---
+++ examples/index.js
@@ -0,0 +1,35 @@
+var animation = document.getElementById('animation');
+var tween = new Tween({ width: 200, height: 100, rgb: 0 })
+  .easing(Tween.Easing.Bounce.Out)
+  .to({ width: 600, height: 300, rgb: [255, 100, 10] }, 1000)
+  .yoyo(true)
+  .delay(1000)
+  .repeat(1)
+  .on('update', function(coords, progress) {
+    animation.style.width = coords.width + 'px';
+    animation.style.height = coords.height + 'px';
+
+    var now = new Date();
+
+    console.log(coords.rgb);
+
+    // console && console.log(
+    //   '%o-%o-%o %o:%o:%o:%o  width: %o  height: %o  progress: %o',
+    //   now.getFullYear(),
+    //   now.getMonth() + 1,
+    //   now.getDate(),
+    //   now.getHours(),
+    //   now.getMinutes(),
+    //   now.getSeconds(),
+    //   now.getMilliseconds(), ~~coords.width, ~~coords.height,
+    //   (~~(progress * 100)) + '%'
+    // );
+  })
+  .start();
+
+function animate(time) {
+  Tween.update(time);
+  requestAnimationFrame(animate);
+}
+
+requestAnimationFrame(animate);
