88656bcb78ed3bd3080f807e35e0b5c629101d45
nuintun <nuintun@qq.com>
2016-11-22 10:02:11 +0800

    update files

--- examples/js/stats.js
+++ examples/js/stats.js
@@ -10,37 +10,28 @@
    */
 
   var Stats = function() {
-
     var mode = 0;
-
     var container = document.createElement('div');
+
     container.style.cssText = 'position:fixed;top:0;left:0;cursor:pointer;opacity:0.9;z-index:10000';
     container.addEventListener('click', function(event) {
-
       event.preventDefault();
       showPanel(++mode % container.children.length);
-
     }, false);
 
     //
-
     function addPanel(panel) {
-
       container.appendChild(panel.dom);
-      return panel;
 
+      return panel;
     }
 
     function showPanel(id) {
-
       for (var i = 0; i < container.children.length; i++) {
-
         container.children[i].style.display = i === id ? 'block' : 'none';
-
       }
 
       mode = id;
-
     }
 
     //
@@ -52,30 +43,20 @@
     var msPanel = addPanel(new Stats.Panel('MS', '#0f0', '#020'));
 
     if (self.performance && self.performance.memory) {
-
       var memPanel = addPanel(new Stats.Panel('MB', '#f08', '#201'));
-
     }
 
     showPanel(0);
 
     return {
-
       REVISION: 16,
-
       dom: container,
-
       addPanel: addPanel,
       showPanel: showPanel,
-
       begin: function() {
-
         beginTime = (performance || Date).now();
-
       },
-
       end: function() {
-
         frames++;
 
         var time = (performance || Date).now();
@@ -83,42 +64,29 @@
         msPanel.update(time - beginTime, 200);
 
         if (time > prevTime + 1000) {
-
           fpsPanel.update((frames * 1000) / (time - prevTime), 100);
 
           prevTime = time;
           frames = 0;
 
           if (memPanel) {
-
             var memory = performance.memory;
             memPanel.update(memory.usedJSHeapSize / 1048576, memory.jsHeapSizeLimit / 1048576);
-
           }
-
         }
 
         return time;
-
       },
-
       update: function() {
-
         beginTime = this.end();
-
       },
-
       // Backwards Compatibility
-
       domElement: container,
       setMode: showPanel
-
     };
-
   };
 
   Stats.Panel = function(name, fg, bg) {
-
     var min = Infinity,
       max = 0,
       round = Math.round;
@@ -134,11 +102,13 @@
       GRAPH_HEIGHT = 30 * PR;
 
     var canvas = document.createElement('canvas');
+
     canvas.width = WIDTH;
     canvas.height = HEIGHT;
     canvas.style.cssText = 'width:80px;height:48px';
 
     var context = canvas.getContext('2d');
+
     context.font = 'bold ' + (9 * PR) + 'px Helvetica,Arial,sans-serif';
     context.textBaseline = 'top';
 
@@ -154,11 +124,8 @@
     context.fillRect(GRAPH_X, GRAPH_Y, GRAPH_WIDTH, GRAPH_HEIGHT);
 
     return {
-
       dom: canvas,
-
       update: function(value, maxValue) {
-
         min = Math.min(min, value);
         max = Math.max(max, value);
 
@@ -175,13 +142,9 @@
         context.fillStyle = bg;
         context.globalAlpha = 0.9;
         context.fillRect(GRAPH_X + GRAPH_WIDTH - PR, GRAPH_Y, PR, round((1 - (value / maxValue)) * GRAPH_HEIGHT));
-
       }
-
     };
-
   };
 
   return Stats;
-
 })));
